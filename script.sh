8d3a5da8631a53106aa3e4d5ffbfdaf27dbb084e
gemppu <>
2022-03-30 21:22:43 +0300

    remove unnecessary stuff

--- script.sh
+++ script.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# episode paths to array
-# for ep
-#   make a new directory
+# make new directory frames
+# for ep in current directory
+#   make new directory inside frames
 #   extract scenes from episode to directory using ffmpeg
-rm -r frames
 mkdir frames
 cd frames
 echo $PWD
-lines=($(ls "$PWD"/../*.mkv))
-echo $lines
 for ep_path in ../*.mkv
 do
   echo "$ep_path"
@@ -16,7 +13,5 @@ do
   echo "$scenes_path"
   mkdir "$scenes_path"
   ffmpeg -i "$ep_path" -filter:v "select='gt(scene,0.9)', showinfo" -vsync 0 "$scenes_path"/%04d.jpg
-
 done
-#ffmpeg -i \[Sephirotic\]\ Evangelion\ -\ 01\ \[MULTI\]\[BD\ 1080p\ 8bits\ 5.1\ AAC\]\[6B1607E9\].mkv -filter:v "select='gt(scene,0.9)',showinfo" -vsync 0 frames/%05d.jpg
 
