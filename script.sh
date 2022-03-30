7825a73b2524a0052232ee38b96e4b6502e956d0
gemppu <>
2022-03-30 21:23:55 +0300

    remove unnecessary echos

--- script.sh
+++ script.sh
@@ -5,12 +5,9 @@
 #   extract scenes from episode to directory using ffmpeg
 mkdir frames
 cd frames
-echo $PWD
 for ep_path in ../*.mkv
 do
-  echo "$ep_path"
   scenes_path=${ep_path:3:-4}_scenes
-  echo "$scenes_path"
   mkdir "$scenes_path"
   ffmpeg -i "$ep_path" -filter:v "select='gt(scene,0.9)', showinfo" -vsync 0 "$scenes_path"/%04d.jpg
 done
